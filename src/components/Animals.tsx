afc0679977fe8d75fa0663fa736fac895bb49e89
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-03-25 16:36:05 +0100

    Console log if animals are fed

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -4,9 +4,32 @@ import { Animal } from "../interfaces/IAnimal"
 
 export const Animals = () => {
     const [animals, setAnimals] = useState<Animal[]>([]);
+    let checkHunger: string = "";
+
+    let lis = animals.map((animal, i) => {
+        return (
+        <li key={i}>
+            <h3>{animal.name}</h3>
+            <img src={animal.imageUrl}></img>
+            </li>);
+    });
+
+    function needsFood() {
+        animals.map((animal,i) => {
+        if (animal.isFed = false) {
+            console.log(animal.name + " måste matas!"); 
+        }
+        else {
+            console.log(animal.name + " är mätt.");    
+        };
+    })
+    };
 
     useEffect(() => {
         localStorage.setItem('animals', JSON.stringify(animals));
+        needsFood();
+        
+
         if(animals.length > 0) return;
 
         axios
@@ -20,10 +43,17 @@ export const Animals = () => {
             });
 
             setAnimals(animalsFromAPI);
+            
         }));
     });
 
     return(
-        <p>Got { animals.length } animals</p>
+        <div>
+            <h1>Välkommen till Djurparken!</h1>
+            <ul>
+                {lis}
+            </ul>
+        
+        </div>
     )
 }
