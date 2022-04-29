e59abdca547aba4ce1f88f249f8017ca68711213
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-04-29 13:29:58 +0200

    Fetching list of animals from API and saving it to LocalStorage

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -1,26 +1,33 @@
-import axios from "axios"
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { IAnimal } from "../interfaces/IAnimal"
+import { IAnimal } from "../interfaces/IAnimal";
 
 export function Animals() {
-    const [animals, setAnimals] = useState<IAnimal[]>([]);
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
 
+  useEffect(() => {
     //Hämta djur från API
+    axios
+      .get<IAnimal[]>("https://animals.azurewebsites.net/api/animals")
+      .then((response) => {
+        //Spara lista med djur i Local Storage
+        if (!localStorage.getItem("animals")) {
+          localStorage.setItem("animals", JSON.stringify(response.data));
+        }
+      });
+  }, []);
 
-    //Spara lista med djur i Local Storage
-
-    //Presentera djur med namn och kort beskrivning
-
-    //Skapa länk för att gå till djurets egen sida
 
 
-    return(
-        <div>
-            <h1>Välkommen till Djurparken!</h1>
-            <ul>
-            </ul>
-        
-        </div>
-    )
+  //Presentera djur med namn och kort beskrivning
+
+  //Skapa länk för att gå till djurets egen sida
+
+  return (
+    <div>
+      <h1>Välkommen till Djurparken!</h1>
+      <ul></ul>
+    </div>
+  );
 }
