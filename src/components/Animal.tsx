cb6556e22d8fd429cc232e8cd5cb24c687f6bc93
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-04-29 16:35:39 +0200

    Animal component displays animals without errors

--- src/components/Animal.tsx
+++ src/components/Animal.tsx
@@ -1,11 +1,37 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom";
+import { IAnimal } from "../interfaces/IAnimal";
 
 export function Animal() {
+  const [animalId, setAnimalId] = useState(0);
+  const [animal, setAnimal] = useState<IAnimal>();
+  let params = useParams();
 
-    return(
-        <div>
-        <Link to={"/"}>Gå tillbaka till startsidan</Link>
-        </div>
-    )
-} 
+  const thisAnimal = JSON.parse(localStorage.getItem("animals") || "[]").find(
+    (animal: { id: number }) => animal.id == animalId
+  );
+
+  useEffect(() => {
+    if (params.id) {
+      setAnimalId(parseInt(params.id));
+    }
+  }, []);
+
+  useEffect(() => {
+    setAnimal(thisAnimal);
+  }, [animalId]);
+
+  function feedAnimal() {
+    console.log(thisAnimal.name, " är matad");
+  }
+
+  return (
+    <div>
+      <Link to={"/"}>Gå tillbaka till startsidan</Link>
+      <img src={thisAnimal?.imageUrl}></img>
+      <p>Välkommen till {animal?.name}!</p>
+      <p>{animal?.shortDescription}</p>
+      <button onClick={feedAnimal}>Mata {animal?.name}</button>
+    </div>
+  );
+}
